4a8700b6bbed8be191699f5ba35f2c499cf0181f
gyureal <yonggyu.jeong10@gmail.com>
2023-09-30 14:13:35 +0900

    feat: reducer 추가 및 booklist container 추가

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'JavaScript: The God'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Knight'},
+        {title: 'eloquent Ruby'}
+    ]
+}
